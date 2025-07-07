2e5099204f2f26c8bd36392787565a95ef808fee
amashamaduwanthi <amashamaduwanthi123@gmail.com>
2025-07-07 11:13:50 +0530

    Add filter by region to Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ import CountryCard from '../components/CountryCard';
 const Home = () => {
     const [countries, setCountries] = useState([]);
     const [searchTerm, setSearchTerm] = useState('');
+    const [selectedRegion, setSelectedRegion] = useState('All');
     const [error, setError] = useState(null);
 
     useEffect(() => {
@@ -19,15 +20,19 @@ const Home = () => {
             });
     }, []);
 
-    // Filter countries based on search input
-    const filteredCountries = countries.filter((country) =>
-        country.name.common.toLowerCase().includes(searchTerm.toLowerCase())
-    );
+    //Filter logic (by name and region)
+    const filteredCountries = countries.filter((country) =>{
+        const matchesName = country.name.common.toLowerCase().includes(searchTerm.toLowerCase());
+        const matchesRegion = selectedRegion === 'All' || country.region === selectedRegion;
+        return matchesName && matchesRegion;
+        });
+    //  Available region options (you can customize this list)
+    const regions = ['All', 'Africa', 'Americas', 'Asia', 'Europe', 'Oceania', 'Antarctic'];
 
     return (
         <div className="p-6 bg-gray-100 min-h-screen">
             <h1 className="text-3xl font-bold mb-6">REST Countries Explorer</h1>
-
+            <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between gap-4 mb-6">
             <input
                 type="text"
                 placeholder="Search by country name..."
@@ -35,6 +40,18 @@ const Home = () => {
                 onChange={(e) => setSearchTerm(e.target.value)}
                 className="mb-6 px-4 py-2 border rounded w-full sm:w-1/2"
             />
+                <select
+                    value={selectedRegion}
+                    onChange={(e) => setSelectedRegion(e.target.value)}
+                    className="px-4 py-2 border rounded w-full sm:w-1/4"
+                >
+                    {regions.map((region) => (
+                        <option key={region} value={region}>
+                            {region}
+                        </option>
+                    ))}
+                </select>
+            </div>
 
             {error && <p className="text-red-500">{error}</p>}
 
